3a7bd40c777767aa83eeae4b333012d7434ddcf9
fuli.shen <fuli.shen@9icaishi.net>
2019-02-20 23:20:17 +0800

    add text classifier

---
+++ NLP_project/3.chinese_text_classifier.py
@@ -0,0 +1,68 @@
+# -*- coding: UTF-8 -*-
+
+# 导入库
+import jieba
+import pandas as pd
+
+#停用词
+stopwords_data_path = "data/stopwords.txt"
+stopwords_data_df = pd.read_csv(stopwords_data_path,encoding="utf-8",sep="\t",index_col=None,quoting=3,names=["stopword"])
+
+# 准备数据
+df_technology = pd.read_csv("./data/technology_news.csv", encoding='utf-8', names=["id", "content"], header=0)
+df_technology = df_technology.dropna()
+
+df_car = pd.read_csv("./data/car_news.csv", encoding='utf-8', names=["id", "content"], header=0)
+df_car = df_car.dropna()
+
+df_entertainment = pd.read_csv("./data/entertainment_news.csv", encoding='utf-8', names=["id", "content"], header=0)
+df_entertainment = df_entertainment.dropna()
+
+df_military = pd.read_csv("./data/military_news.csv", encoding='utf-8', names=["id", "content"], header=0)
+df_military = df_military.dropna()
+
+df_sports = pd.read_csv("./data/sports_news.csv", encoding='utf-8', names=["id", "content"], header=0)
+df_sports = df_sports.dropna()
+
+stopwords = stopwords_data_df.stopword.values.tolist()
+technology = df_technology.content.apply(lambda line: line.strip()).values.tolist()
+car = df_car.content.apply(lambda line: line.strip()).values.tolist()
+entertainment = df_entertainment.content.apply(lambda line: line.strip()).values.tolist()
+military = df_military.content.apply(lambda line: line.strip()).values.tolist()
+sports = df_sports.content.apply(lambda line: line.strip()).values.tolist()
+
+# 分词和中文处理
+'''
+5类数据最终处理的格式：
+word1 word2 word3 technology 
+word1 word2 word3 word4 technology 
+word1 word2 word4 car 
+'''
+
+'''
+    lines: 文章的一条记录
+    sentences: 返回的数据列表
+    category： 文章的类别
+'''
+def preprocess_text(lines, sentences, category):
+    for line in lines[0:10]:
+        segs = jieba.lcut(line)
+        segs = [seg for seg in segs if len(seg) > 1 and seg not in stopwords]
+        data = " ".join(segs)
+        label = category
+        sentences.append((data, label))
+
+
+sentences = []
+preprocess_text(technology, sentences, "technology")
+preprocess_text(car, sentences, "car")
+preprocess_text(entertainment, sentences, "entertainment")
+preprocess_text(military, sentences, "military")
+preprocess_text(sports, sentences, "sports")
+
+
+## 样本顺序打乱 并打印训练格式的样本
+import random
+random.shuffle(sentences)
+for data_label in sentences:
+    print(data_label[1] + " " + data_label[0])
